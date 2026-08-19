89928315e7cb4d7e49b5e1aa0b09850e0a13cf83
dynastxu <151742029+ETOofficial@users.noreply.github.com>
2026-08-19 22:22:58 +0800

    修正 DiscBullet 伤害逻辑

--- src/main/java/dynastxu/noitacore/entity/projectile/DiscBullet.java
+++ src/main/java/dynastxu/noitacore/entity/projectile/DiscBullet.java
@@ -1,10 +1,19 @@
 package dynastxu.noitacore.entity.projectile;
 
+import dynastxu.noitacore.common.spell.DamageType;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.level.Level;
 
 public class DiscBullet extends SpellProjectile{
     public DiscBullet(EntityType<? extends SpellProjectile> type, Level level) {
         super(type, level);
+    }
+
+    @Override
+    protected void hurtEntity(Entity entity) {
+        float damage = damageMap.getOrDefault(DamageType.Slice, 0f);
+        damageMap.put(DamageType.Slice, calculateDamageDependsOnSpeed(damage));
+        super.hurtEntity(entity);
     }
 }
